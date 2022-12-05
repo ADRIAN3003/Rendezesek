66b63afdc3d58cafde2e6ab7e39d1f2e675797d3
Medve Adrián <medveadrian.szgya@gmail.com>
2022-12-05 13:00:20 +0100

    fix beszuras

---
+++ Beszuro/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beszuro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[80000];
+
+            Beolvas(t);
+
+            Stopwatch stopper = new Stopwatch();
+
+            Console.WriteLine("Beszúró rendezés");
+
+            stopper.Start();
+
+            for (int i = 1; i < t.Length; i++)
+            {
+                int j = i - 1;
+                while (j >= 0 && t[j] > t[j + 1])
+                {
+                    int temp = t[j];
+                    t[j] = t[j + 1];
+                    t[j + 1] = temp;
+                    j = j - 1;
+                }
+            }
+
+            stopper.Stop();
+
+            //foreach (var item in t)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            Console.WriteLine(stopper.ElapsedMilliseconds);
+
+            Console.ReadKey();
+        }
+
+        private static void Beolvas(int[] t)
+        {
+            using (StreamReader sr = new StreamReader("adatok.txt"))
+            {
+                int i = 0;
+                while (!sr.EndOfStream)
+                {
+                    t[i++] = Convert.ToInt32(sr.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ FixBeszuro/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixBeszuro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[80000];
+
+            Beolvas(t);
+
+            Stopwatch stopper = new Stopwatch();
+
+            Console.WriteLine("Javított Beszúró rendezés");
+
+            stopper.Start();
+
+            for (int i = 1; i < t.Length; i++)
+            {
+                int j = i - 1;
+                int tmp = t[i];
+                while (j >= 0 && t[j] > tmp)
+                {
+                    t[j + 1] = t[j];
+                    j = j - 1;
+                }
+                t[j + 1] = tmp;
+            }
+
+            stopper.Stop();
+
+            //foreach (var item in t)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            Console.WriteLine(stopper.ElapsedMilliseconds);
+
+            Console.ReadKey();
+        }
+
+        private static void Beolvas(int[] t)
+        {
+            using (StreamReader sr = new StreamReader("adatok.txt"))
+            {
+                int i = 0;
+                while (!sr.EndOfStream)
+                {
+                    t[i++] = Convert.ToInt32(sr.ReadLine());
+                }
+            }
+        }
+    }
+}
