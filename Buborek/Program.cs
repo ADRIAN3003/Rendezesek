e28af1a37ce87f129932fbe47b5e635c99e92084
Medve Adrián <medveadrian.szgya@gmail.com>
2022-12-05 09:57:30 +0100

    Add project files.

---
+++ Buborek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buborek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[] { 4, 5, 8, 2, 3, 9, 2, 6, 1 };
+
+            Console.WriteLine("Buborék rendezés");
+
+            for (int i = t.Length - 1; i >= 1; i--)
+            {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    if (t[j] > t[j + 1])
+                    {
+                        int temp = t[j];
+                        t[j] = t[j + 1];
+                        t[j + 1] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in t)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EgyszeruCsere/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace EgyszeruCsere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[40000];
+
+            Stopwatch stopper = new Stopwatch();
+
+            Beolvas(t);
+
+            Console.WriteLine("Egyszerű csere rendezés");
+
+            stopper.Start();
+
+            for (int i = 0; i < t.Length - 1; i++)
+            {
+                for (int j = i + 1; j < t.Length; j++)
+                {
+                    if (t[i] > t[j])
+                    {
+                        int temp = t[i];
+                        t[i] = t[j];
+                        t[j] = temp;
+                    }
+                }
+            }
+
+            stopper.Stop();
+
+            //foreach (var item in t)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            Console.WriteLine(stopper.ElapsedMilliseconds);
+
+            Console.ReadLine();
+        }
+
+        private static void Beolvas(int[] t)
+        {
+            using (StreamReader sr = new StreamReader("adatok.txt"))
+            {
+                int i = 0;
+                while (!sr.EndOfStream)
+                {
+                    t[i++] = Convert.ToInt32(sr.ReadLine());
+                }
+            }
+        }
+    }
+}
